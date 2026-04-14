69896ed28eb75f89e6b1462827be9daad51fab92
C Frovik <christer.frovik@gmail.com>
2026-04-14 19:47:36 +0200

    Add PWA support to app folders

---
+++ NaughtAndCrosses_JS/sw.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var CACHE_NAME = 'naught-and-crosses-js-v1';
+var APP_SHELL = [
+  './',
+  './NaughtsAndCrosses_JS.html',
+  './manifest.webmanifest',
+  './pwa-icon.svg'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        if (key !== CACHE_NAME) return caches.delete(key);
+        return Promise.resolve();
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  if (new URL(event.request.url).origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      if (cached) return cached;
+      return fetch(event.request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('./NaughtsAndCrosses_JS.html');
+      });
+    })
+  );
+});
