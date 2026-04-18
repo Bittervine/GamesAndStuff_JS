7820cbadaaa8fe971b4ddd9babb0262ce3979698
CJFrovik <bittervine@gmail.com>
2026-04-18 22:45:02 +0200

    Add ShotEmUp3D assets and unpacker updates

---
+++ ShotEmUp3D_JS/proj_unpatched/sw.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var CACHE_NAME = 'shotemup-js-v3';
+var APP_SHELL = [
+  './',
+  './ShotEmUp_JS.html',
+  './manifest.webmanifest',
+  './pwa-icon.svg'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        if (key !== CACHE_NAME) return caches.delete(key);
+        return Promise.resolve();
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  var url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (url.pathname === '/ShotEmUp_JS/ShotEmUp_JS.html' || url.pathname === '/ShotEmUp_JS/ShotEmUp_JS.js') {
+    event.respondWith(
+      fetch(event.request, { cache: 'no-store' }).then(function (response) {
+        if (response && response.ok) {
+          var copy = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, copy);
+          });
+        }
+        return response;
+      }).catch(function () {
+        return caches.match(event.request).then(function (cached) {
+          if (cached) return cached;
+          return caches.match('./ShotEmUp_JS.html');
+        });
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      if (cached) return cached;
+      return fetch(event.request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('./ShotEmUp_JS.html');
+      });
+    })
+  );
+});
