459accacf7d5c8b9f29ba51ad0801b37e6451853
CJFrovik <bittervine@gmail.com>
2026-05-09 03:29:09 +0200

    Bump ThoriumGap asset cache version to v25

--- ThoriumGap_JS/sw.js
+++ ThoriumGap_JS/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var APP_VERSION = 'thoriumgap-v24'; // BUMP ME
+var APP_VERSION = 'thoriumgap-v25'; // BUMP ME
 var CACHE_NAME = APP_VERSION;
 var APP_SHELL = [
   './ThoriumGap.html',
