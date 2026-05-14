4feaa3b3aaf29123ac75a639b2270c374467948e
CJFrovik <bittervine@gmail.com>
2026-05-14 16:08:32 +0200

    ThoriumGap: bump service worker cache version to v33

--- ThoriumGap_JS/sw.js
+++ ThoriumGap_JS/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var APP_VERSION = 'thoriumgap-v32'; // BUMP ME
+var APP_VERSION = 'thoriumgap-v33'; // BUMP ME
 var CACHE_NAME = APP_VERSION;
 var APP_SHELL = [
   './ThoriumGap.html',
