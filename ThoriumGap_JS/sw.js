cc6e62ac3b0c5cbd3613ee70ceb95b1d3dcb9764
CJFrovik <bittervine@gmail.com>
2026-06-14 03:29:31 +0200

    Bump Thorium Gap service worker cache

--- ThoriumGap_JS/sw.js
+++ ThoriumGap_JS/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var APP_VERSION = 'thoriumgap-v59'; // BUMP ME
+var APP_VERSION = 'thoriumgap-v93'; // BUMP ME
 var CACHE_NAME = APP_VERSION;
 var APP_SHELL = [
   './ThoriumGap.html',
